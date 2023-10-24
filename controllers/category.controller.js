a8972755540a8df88ec06e441b65af58e4e5ba53
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-10-24 12:41:53 +0545

    get, delete, update, create category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -5,7 +5,6 @@ import asyncHandler from "express-async-handler";
  * @desc   Create a new category
  * @route  POST /api/v1/categories
  * @access Private/Admin
- 
  *****************************************************/
 
 export const createCategory = asyncHandler(async (req, res) => {
@@ -25,5 +24,89 @@ export const createCategory = asyncHandler(async (req, res) => {
   res.status(201).json({
     category,
     msg: "Category created successfully",
+  });
+});
+
+/*****************************************************
+ * @desc   Get all category
+ * @route  GET /api/v1/categories
+ * @access PUblic
+ *****************************************************/
+
+export const getAllCategory = asyncHandler(async (req, res) => {
+  const category = await Category.find({});
+
+  res.status(200).json({
+    length: category.length,
+    category,
+    msg: "All category",
+  });
+});
+
+/*****************************************************
+ * @desc   Get single category
+ * @route  GET /api/v1/categories/:id
+ * @access PUblic
+ *****************************************************/
+
+export const getSingleCategory = asyncHandler(async (req, res) => {
+  // get single category
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    throw new Error("Category not found");
+  }
+
+  res.status(200).json({
+    category,
+    msg: "Single category",
+  });
+});
+
+/*****************************************************
+ * @desc   update category
+ * @route  PUT /api/v1/categories/:id
+ * @access PRIVATE/ADMIN
+ *****************************************************/
+
+export const updateCategory = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+  const category = await Category.findById(req.params.id);
+  if (!category) {
+    throw new Error("Category not found");
+  }
+
+  await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+    },
+    { new: true, runValidators: true }
+  );
+
+  await category.save();
+
+  res.status(200).json({
+    category,
+    msg: "Category updated successfully",
+  });
+});
+
+/*****************************************************
+ * @desc   Delete category
+ * @route  DELETE /api/v1/categories/:id
+ * @access PRIVATE/ADMIN
+ *****************************************************/
+
+export const deleteCategory = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.params.id);
+  if (!category) {
+    throw new Error("Category not found");
+  }
+
+  await category.deleteOne();
+
+  res.status(200).json({
+    msg: "Category deleted successfully",
   });
 });
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
 
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
-import { createCategory } from "../controllers/category.controller.js";
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategory,
+  getSingleCategory,
+  updateCategory,
+} from "../controllers/category.controller.js";
 const router = express.Router();
 
-router.route("/categories").post(isLoggedIn, createCategory) 
-
+router
+  .route("/categories")
+  .post(isLoggedIn, createCategory)
+  .get(getAllCategory);
+router
+  .route("/categories/:id")
+  .get(getSingleCategory)
+  .put(isLoggedIn, updateCategory)
+  .delete(isLoggedIn, deleteCategory);
 
 export default router;
